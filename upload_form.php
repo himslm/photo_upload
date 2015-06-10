0d1f33b50f65178e3993ba46faba75407838751a
Matt H. <himslm@gmail.com>
2015-06-10 10:02:55 -0500

    Create upload_form.php

---
+++ upload_form.php
@@ -0,0 +1,9 @@
+<form method="post" action="processes.php" enctype="multipart/form-data">
+  <input type="hidden" name="gallery_ID" value="<?php echo $_GET['gallery_ID']; ?>" />
+  <table border="0" cellpadding="5" cellspacing="0">
+    <tr>
+      <td><label>Add Photos:</label></td>
+      <td><input type="file" name="photo[]" multiple="multiple" /></td>
+    </tr>
+  </table>
+</form>
